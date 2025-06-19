ef02cda8f3c7d2ecdb74a91c6f9be2633e355bd5
Sakshi Dewangan <sakshidewangan51@gmail.com>
2025-06-19 17:26:29 +0530

    update 1

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), require("autoprefixer")],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
