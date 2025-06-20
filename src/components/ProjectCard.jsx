14e2b30198ae8af100a7c544d02e9cfd14a90bbf
Sakshi Dewangan <sakshidewangan51@gmail.com>
2025-06-20 14:59:02 +0530

    update frame motion

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -4,7 +4,6 @@ import { CardBody, CardContainer, CardItem } from "./ui/3d-card";
 
 const ProjectCard = ({ number, role, title, images, liveUrl }) => {
   return (
-    // ⬇️ Make this wrapper sticky, not inside CardBody
     <div className="sticky-project-card-wrapper">
       <CardContainer className="inter-var">
         <CardBody className="bg-gray-50 relative group/card dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border">
